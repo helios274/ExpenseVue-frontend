5a5bbfc93e3538b1c6a66558991fa73d50c9cffd
Adithya Prasad <adithyasridharprasad@gmail.com>
2023-10-28 11:55:17 +0530

    Styling updated 2

--- src/Layout..jsx
+++ src/Layout..jsx
@@ -3,10 +3,16 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { useSelector } from "react-redux";
 
 function Layout() {
+  const { userData } = useSelector((state) => state.auth);
   return (
-    <div className="flex flex-col min-h-screen px-2 sm:px-4 md:px-12 lg:px-28 xl:px-40 background">
+    <div
+      className={`flex flex-col min-h-screen px-2 sm:px-4 md:px-12 lg:px-28 xl:px-40 ${
+        userData ? "" : "background"
+      }`}
+    >
       <Header />
       <main className="mt-2 sm:mt-3 lg:mt-4 mb-2 sm:mb-3 lg:mb-4 flex-grow">
         <ToastContainer
